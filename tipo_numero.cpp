010a5c0b1a4d8ac194acc63ea36a10c9cfa36f3a
Lourdes Jiménez <00090220@uca.edu.sv>
2025-06-20 15:49:31 -0600

    tipo numero n

--- tipo_numero.cpp
+++ tipo_numero.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+
+int main() {
+  int numero = 0;
+
+  //Solicita al usuario ingresar un numero entero
+  std::cout << "Ingresa un numero entero: ";
+  std::cin >> numero;
+
+  //Determina si el numero es positivo, negativo o cero
+  if (numero > 0) {
+    std::cout << "El numero ingresado es: Positivo" << std::endl;
+  } else if (numero < 0) {
+    std::cout << "El numero ingresado es: Negativo" << std::endl;
+  } else {
+    std::cout << "El numero ingresado es: Cero" << std::endl;
+  }
+
+  return 0;
+}
 
